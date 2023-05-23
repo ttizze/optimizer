1aa63eded49f020d8c36a3ae831f61a90ef4aff2
john <john@gmail.com>
2023-05-23 23:38:42 +0900

    lineがトークンは一度しか使えないので変更

--- main.py
+++ main.py
@@ -81,25 +81,28 @@ def handle_message(event):
             DBLayer().create_user(line_user_id)
             user_id = DBLayer().get_user_id_from_line_id(line_user_id)
             personal_information = linebot.get_personal_information_from_api()
+
+            messages = []
             first_message = linebot.first_message(personal_information)
-            line_bot_api.reply_message(event.reply_token, first_message)
+            messages.append(first_message)
             profile = linebot.second_message(personal_information)
-            line_bot_api.reply_message(event.reply_token, profile)
+            messages.append(profile)
             DBLayer().update_profile(user_id, profile.text)
-            print(profile)
             result = linebot.search_support(profile)
-            line_bot_api.reply_message(event.reply_token, result)
-            print(result.text)
-            line_bot_api.reply_message(event.reply_token, linebot.after_search_result())
+            after_search_result = linebot.after_search_result()
+            messages.append(after_search_result)
+            line_bot_api.reply_message(event.reply_token, messages)
         else:
             line_bot_api.reply_message(event.reply_token, linebot.agreement_message())
     else:
+        messages = []
         line_bot_obj = linebot.update_profile(user_id, user_message)
+        messages.append(line_bot_obj)
         DBLayer().update_profile(user_id, line_bot_obj.text)
-        line_bot_api.reply_message(event.reply_token, line_bot_obj)
-        print(line_bot_obj.text)
         result = linebot.search_support(line_bot_obj.text)
-        line_bot_api.reply_message(event.reply_token, result)
-        line_bot_api.reply_message(event.reply_token, linebot.after_search_result())
+        messages.append(result)
+        after_search_result = linebot.after_search_result()
+        messages.append(after_search_result)
+        line_bot_api.reply_message(event.reply_token, messages)
 
 
